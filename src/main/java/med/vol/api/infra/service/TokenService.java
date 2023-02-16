437f0d6a9ef46c6785619c4f4026cc2d956d7b12
Andrade <vicente.andrade@capgemini.com>
2023-02-16 20:57:51 -0300

    ajustando Value do TokenService

--- src/main/java/med/vol/api/infra/service/TokenService.java
+++ src/main/java/med/vol/api/infra/service/TokenService.java
@@ -13,7 +13,7 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-    @Value("api.security.token.secret")
+    @Value("${api.security.token.secret}")
     private String secret;
 
     public String generateToken(UsuarioModel usuario) {
